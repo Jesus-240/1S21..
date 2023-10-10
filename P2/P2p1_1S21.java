7004574ae650717a4c5acad271e9c989a96d2fed
Jesus Ezequiel Arroyo Rodriguez <144179425+Jesus-240@users.noreply.github.com>
2023-10-10 15:01:39 -0600

    Add files via upload

---
+++ P2/P2p1_1S21.java
@@ -0,0 +1,55 @@
+public class P2p1_1S21{
+	public static void main(String[] args){
+		int a=12;
+		if (a<10){
+			System.out.println("Si ingreso por que es verdadero");
+		}
+
+		if (a>10){
+			System.out.println("este segmento si se ejecuta porque se cumple con la condicion");
+		}
+
+		if (true){
+			System.out.println("aqui se ejecuta por que es verdadero");
+		}
+
+		if (false){
+			System.out.println("esto no se ejecuta por que es falso");
+		}
+
+		if ((a>10) == true){
+			System.out.println("se ejecutara?");
+		}
+		System.out.println("continuamos con el codigo");
+
+		System.out.println("");
+
+		a= 18;
+
+		if (a>17){
+			System.out.println("Eres mayor de edad");
+		} else {
+			System.out.println("Eres menor de edad");
+		}
+
+		a=2;
+
+		if (a == 1){
+			System.out.println("se evaluo a 1");
+		}else if (a == 2){
+			System.out.println("se evaluo a 2");
+		}else if (a ==3){
+			System.out.println("se evaluo a 3");
+		}else{
+			System.out.println("se evaluaron los demas digitos");
+		}
+
+		if (divisor == 0){
+			System.out.println("No es posble hacer la division");
+		}else{
+			System.out.println("Si puedo realizar la division");
+		}
+		
+
+	}
+}
