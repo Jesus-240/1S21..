766c0a52c8336dfbb0bc17ccf970b89732a6c835
Jesus Ezequiel Arroyo Rodriguez <144179425+Jesus-240@users.noreply.github.com>
2023-10-19 22:56:24 -0600

    Add files via upload

---
+++ P2/P2p3_1S21.java
@@ -0,0 +1,12 @@
+public class P2p3_1S21 {
+	public static void main(String[] args){
+	  for(int a=0;a<=10;a++){ 
+	System.out.println("Tabla de multiplicar del " + a + ":");
+	  for(int b=1; b<=10;b++){
+	    int resultado= a*b;
+	System.out.println( a + "x" + b + "=" + resultado);
+	  }
+	System.out.println();
+	  }
+	}
+}
